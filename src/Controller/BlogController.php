b69d7cdedb20043cdc6df8374e771cbd15fc56da
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-06-21 01:57:06 +0200

    detail post

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,17 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/index.html.twig', [
             'title' => 'Blog',
+        ]);
+    }
+    
+    /**
+     * @Route("/blog/{id}/detail", name="blog_detail")
+     */
+    public function detail(Post $post): Response
+    {
+        return $this->render('blog/detail.html.twig', [
+            'title' => 'Blog',
+            'post'=>$post
         ]);
     }
 }
