8ecf1978f8be4246e6b4157a213f6cef9484aa43
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-06-21 02:20:56 +0200

    mise en place du dasbord

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -21,7 +21,19 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // redirect to some CRUD controller
+        //$routeBuilder = $this->get(AdminUrlGenerator::class);
+
+       // return $this->redirect($routeBuilder->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // you can also redirect to different pages depending on the current user
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // you can also render some template to display a proper Dashboard
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        return $this->render('dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
